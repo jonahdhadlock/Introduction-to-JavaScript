493279a750bac2d05e78ba164a5a706846f71eff
Jonah D. Hadlock <jonah-hadlock@lambdastudents.com>
2020-08-21 17:34:16 -0600

    finished the first task

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
-
-
-
+var votingAge = 20;
+if (votingAge > 18) {
+    console.log("True")
+}
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
-
 
 
 
@@ -48,7 +48,7 @@
 // 7 - 12 months 4% of their body weight
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
-  
+
 
 
 
@@ -60,8 +60,8 @@
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-  
-  
+
+
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
@@ -72,7 +72,7 @@
 
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
-  
+
 
 
 
@@ -81,7 +81,7 @@
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-  
+
 
 
 
@@ -94,10 +94,10 @@
 //70s should be Cs 
 //60s should be D 
 //and anything below 60 should be F
-  
 
-  
-  
+
+
+
 
 /************************************************************** Stretch **************************************************************/
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
@@ -111,8 +111,3 @@
 /************************************************************** Stretch **************************************************************/
 //Take Rock, Paper, Sissors further
 //update your rock papers sissors code below to take a prompt from a user using the window object
-
-
-
-
-
